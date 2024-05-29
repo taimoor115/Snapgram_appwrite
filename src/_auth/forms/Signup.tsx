85e2e7bbf9a287231fdc8840c138ca4713bc5a50
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-05-29 15:32:56 +0500

    Crate Sign Up Form

--- src/_auth/forms/Signup.tsx
+++ src/_auth/forms/Signup.tsx
@@ -1,10 +1,128 @@
 import { Button } from "@/lib/utils/ui/button";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/lib/utils/ui/form";
+import { Input } from "@/lib/utils/ui/input";
+import { SignupSchema } from "@/lib/validation";
+import { z } from "zod";
+import { Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+
 const Signup = () => {
+  const isLoading = false;
+  const form = useForm<z.infer<typeof SignupSchema>>({
+    resolver: zodResolver(SignupSchema),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SignupSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <Form {...form}>
+      <div className="flex-col justify-center sm:w-420 ">
+        <img src="/assets/images/logo.svg" alt="" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12 ">
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular">
+          To use Snapgram, please enter your details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input className="shad-input" type="text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input className="shad-input" type="text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input className="shad-input" type="email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input className="shad-input" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader />
+                Loading...
+              </div>
+            ) : (
+              "Sign Up"
+            )}
+          </Button>
+          <p className="text-center text-small-regular text-light-2 mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Log in
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
   );
 };
 
